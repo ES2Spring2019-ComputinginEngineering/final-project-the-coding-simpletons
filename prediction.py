a9a4f0e59cfb2b448ab87c160a01e1d1485ce0dd
ronangiss <ronan@sept.gmail.com>
2019-04-30 20:10:34 -0400

    Create prediction.py

---
+++ prediction.py
@@ -0,0 +1,44 @@
+#Ben and Rónán
+
+import numpy as np
+
+def trending(values):
+    change = []
+    length = values.size
+    final_day = values[length - 1]
+    for i in range(length - 2):
+        delta = (values[i+1] - values[i])
+        change.append(delta)
+    avechange = np.mean(change)
+    next_day = final_day + avechange
+    return next_day
+
+def nearest_neighbor(type_1, type_2, percip, new_1, new_2):
+    distance_arr = np.zeros(type_1.size)
+    for i in range(type_1.size):
+        distance_squared = (((type_1[i]-new_1)**2)+(type_2[i]-new_2)**2)
+        distance = np.sqrt(distance_squared)
+        distance_arr[i] = distance
+    smallesti = np.argmin(distance_arr)
+    raining = int(percip[smallesti])
+    return raining
+
+
+#Instead of Using Median we could use Percentage of nearest points that are rain to predict likelihood
+def kNearestNeighborClassifier(type_1, type_2, percip, new_1, new_2):
+    closest = np.zeros(5)
+    distance_arr = np.zeros(type_1.size)
+    for i in range(type_1.size):
+        distance_squared = (((type_1[i]-new_1)**2)+(type_2[i]-new_2)**2)
+        distance = np.sqrt(distance_squared)
+        distance_arr[i] = distance
+    indexes = np.argsort(distance_arr)
+    for i in range(5):
+        closest[i] = percip[indexes[i]] 
+    raining = int(np.median(closest))
+    rain = 0
+    for i in range(closest.size):
+        if closest[i] == 1:
+            rain += 1
+    percentage = rain / 5
+    return raining, percentage
