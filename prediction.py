098be95789c3f0fcd69e5ebd924d9c6f1767d9ed
Ben Pradko <Benjamin.Pradko@tufts.edu>
2019-05-03 15:39:59 -0400

    working all day

--- prediction.py
+++ prediction.py
@@ -1,14 +1,14 @@
 #Ben and Rónán
 
 import numpy as np
-
+'''
 def tomorrow(indexes, dates, data):
     weights=[1, ((np.e)**(1/2)),np.e,((np.e)**(3/2)), ((np.e)**(2)),((np.e)**(5/2)),((np.e)**(3))]
     weighted=[]
     for i in indexes:
         if dates
     return 
-
+'''
 #SOMEHOW CHECK WHICH DAYS THESE HOURLY VALUES ARE ASSOCIATED WITH? DO WE HAVE ONE WEEK REPRESENTED?
         
 
