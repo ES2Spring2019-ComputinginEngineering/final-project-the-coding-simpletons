3bbfff49ce8bb20e3114b55cf75e9a6932fd28db
Ben Pradko <Benjamin.Pradko@tufts.edu>
2019-05-04 14:36:38 -0400

    error prone

--- main.py
+++ main.py
@@ -8,17 +8,27 @@ might want to test this on a classmates computer to be sure it works!"""
 # IMPORT STATEMENTS
 import data 
 import clustering
-from presentation import *
+import presentation
 import tkinter as tk
-
+import prediction
 
 # DEMONSTRATION CODE
 
+    #Variable assignments
+bestDateSet, time, hourlytemp, hourlyprecip, hourlyseapress, hourlyhum, hourlyVis, hourlyPeakWind, hourlyWind, nhourlytemp, nhourlyprecip, nhourlyseapress, nhourlyhum, nhourlyVis, nhourlyPeakWind, nhourlyWind = data.readDataFile()
+dateRange = data.sliceOfInterest(bestDateSet, time)
+btime, bhourlytemp, bhourlyprecip, bhourlyseapress, bhourlyhum, bhourlyVis, bhourlyPeakWind, bhourlyWind, bnhourlytemp, bnhourlyprecip, bnhourlyseapress, bnhourlyhum, bnhourlyVis, bnhourlyPeakWind, bnhourlyWind = data.dataOfInterest(dateRange, time, hourlytemp, hourlyprecip, hourlyseapress, hourlyhum, hourlyVis, hourlyPeakWind, hourlyWind, nhourlytemp, nhourlyprecip, nhourlyseapress, nhourlyhum, nhourlyVis, nhourlyPeakWind, nhourlyWind)
+K = 2
+    #COLLECTING PREDICTED VALUES
+humidity, pressure, visibility, temperature, winds, Nhumidity, Nvisibility, Npressure = prediction.tomorrow(bestDateSet, btime, bhourlyhum, bhourlyseapress, bhourlyVis, bhourlytemp, bhourlyWind, bnhourlyhum, bnhourlyVis, bnhourlyseapress)
+rain_value, rain_percent = prediction.kNearestNeighborClassifier(hourlyhum, hourlyseapress, hourlyVis, hourlyprecip, Nhumidity, Npressure, Nvisibility)
+
+
 print('The best set of days ranges from:\n' + str(bestDateSet[0]) + ' to ' + str(bestDateSet[len(bestDateSet)-1]))
 
-data.graphData3D(nhourlyhum, nhourlyseapress, nhourlyVis, nhourlyprecip, Nnexthum, Nnextvis, Nnextpress)
+data.graphData3D(nhourlyhum, nhourlyseapress, nhourlyVis, nhourlyprecip, Nhumidity, Nvisibility, Npressure)
 
-K=2
+
 centroids = clustering.create_centroids(K)
 
 final_centroids, assignments = clustering.iteration(centroids, nhourlyhum, nhourlyseapress, nhourlyVis)
@@ -27,4 +37,4 @@ clustering.graphing(nhourlyhum, nhourlyVis, nhourlyseapress, final_centroids, as
 
 
 root = tk.Tk()
-interface(master=root).mainloop()
+presentation.interface(master=root).mainloop()
