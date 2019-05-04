5706859d21656a0496866fb973d08a0f3753aaec
Ben Pradko <Benjamin.Pradko@tufts.edu>
2019-05-04 13:18:40 -0400

    getting closer?

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import data
 import clustering
 import prediction
 import presentation
+import tkinter as tk
 
 
 # DEMONSTRATION CODE
@@ -22,36 +23,8 @@ btime, bhourlytemp, bhourlyprecip, bhourlyseapress, bhourlyhum, bhourlyVis, bhou
 
 nexthum, nextpress, nextvis, nexttemp, nextwind, Nnexthum, Nnextpress, Nnextvis = prediction.tomorrow(bestDateSet, btime, bhourlyhum, bhourlyseapress, bhourlyVis, bhourlytemp, bhourlyWind, bnhourlyhum, bnhourlyseapress, bnhourlyVis)
 
-<<<<<<< HEAD
 data.graphData3D(nhourlyhum, nhourlyseapress, nhourlyVis, nhourlyprecip, Nnexthum, Nnextvis, Nnextpress)
-=======
-prediction.predictedAccuracy(bhourlyhum, bhourlyseapress, bhourlyVis, bhourlytemp, bhourlyWind, nexthum, nextpress, nextvis, nexttemp, nextwind)
 
-
-#data.graphData3D(nhourlyseapress, nhourlyhum, nhourlyWind, nhourlyVis, nhourlytemp, nhourlyprecip, Nnexthum, Nnextvis, Nnextpress)
->>>>>>> 314d0b2114d7e60ab57eedf8452e95715d130ba8
-
-"""
-K=2
-centroids = clustering.create_centroids(K)
-
-final_centroids, assignments = clustering.iteration(centroids, hourlyhum, hourlyseapress, hourlyVis)
-
-clustering.graphing(K, hourlyhum, hourlyVis, hourlyseapress, final_centroids, assignments)
-"""
-
-# This files should not contain any function defitions
-
-
-# IMPORT STATEMENTS
-
-
-
-
-# DEMONSTRATION CODE
-
-
-data.graphData3D(nhourlyhum, nhourlyseapress, nhourlyVis, nhourlyprecip, Nnexthum, Nnextpress, Nnextvis)
 
 K=2
 centroids = clustering.create_centroids(K)
@@ -60,11 +33,7 @@ final_centroids, assignments = clustering.iteration(centroids, nhourlyhum, nhour
 
 clustering.graphing(nhourlyhum, nhourlyVis, nhourlyseapress, final_centroids, assignments)
 
-"""
-import presentation
-import tkinter as tk
-
 root = tk.Tk()
 presentation.interface(master=root).mainloop()
-"""
+
 
